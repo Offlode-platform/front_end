6eacf26538a677c3fd4249c70b49df52ed35c0b6
zohiaby <zohaiby33@gmail.com>
2026-03-27 14:40:54 +0500

    Update environment configuration to set default API and app URLs to 'https://api.offlode.com' for improved accessibility.

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,4 @@
 export const env = {
-  publicApiUrl: process.env.NEXT_PUBLIC_API_URL ?? "",
-  appUrl: process.env.NEXT_PUBLIC_APP_URL ?? "",
+  publicApiUrl: process.env.NEXT_PUBLIC_API_URL ?? "https://api.offlode.com",
+  appUrl: process.env.NEXT_PUBLIC_APP_URL ?? "https://api.offlode.com",
 } as const;
